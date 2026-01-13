2701337f02d751467922f12db43e79e886a81dc2
Abdelrahman Tarek <A.Khalil@wakeb.tech>
2026-01-13 13:41:38 +0200

    wip

---
+++ bootstrap/app.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        api: __DIR__.'/../routes/api/routes.php',
+        commands: __DIR__.'/../routes/console/routes.php',
+        channels: __DIR__.'/../routes/channels/routes.php',
+        health: '/up',
+        apiPrefix: '',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
