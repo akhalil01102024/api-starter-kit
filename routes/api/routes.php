90da291e50f429c3708beacf73ca03a6a343c4e1
Abdelrahman Tarek <A.Khalil@wakeb.tech>
2026-01-21 10:26:15 +0200

    wip

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -7,4 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home');
 
 /** V1 */
-Route::prefix('auth')->name('auth.')->group(base_path(path: 'routes/api/v1/routes.php'));
+Route::prefix('v1')->name('v1.')->group(base_path(path: 'routes/api/v1/routes.php'));
