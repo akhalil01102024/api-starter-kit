2f7d798cdbedbb70ffa880205b798572332487b4
Abdelrahman Tarek <A.Khalil@wakeb.tech>
2026-01-22 10:47:10 +0200

    wip

--- app/Http/Resources/V1/Auth/UserResource.php
+++ app/Http/Resources/V1/Auth/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\V1\Auth;
 
+use App\Http\Resources\V1\Common\CarbonResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -20,9 +21,9 @@ class UserResource extends JsonResource
             'id' => $this->resource->id,
             'name' => $this->resource->name,
             'email' => $this->resource->email,
-            'email_verified_at' => $this->resource->email_verified_at?->toIso8601String(),
-            'created_at' => $this->resource->created_at?->toIso8601String(),
-            'updated_at' => $this->resource->updated_at?->toIso8601String(),
+            'email_verified_at' => CarbonResource::make(resource: $this->resource->email_verified_at),
+            'created_at' => CarbonResource::make(resource: $this->resource->created_at),
+            'updated_at' => CarbonResource::make(resource: $this->resource->updated_at),
         ];
     }
 }
