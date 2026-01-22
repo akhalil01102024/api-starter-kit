c2110ffdf9e55dbd8dc6fae50c01fed2516b5827
Abdelrahman Tarek <A.Khalil@wakeb.tech>
2026-01-22 10:26:23 +0200

    wip

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
-/** Home */
-Route::get('/', HomeController::class)->name('home');
 
 /** V1 */
 Route::prefix('v1')->name('v1.')->group(base_path(path: 'routes/api/v1/routes.php'));
--- routes/api/v1/routes.php
+++ routes/api/v1/routes.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\V1\HomeController;
 use Illuminate\Support\Facades\Route;
-
-/** Home */
-Route::get('/', HomeController::class)->name('home');
 
 /** Auth */
 Route::prefix('auth')->name('auth.')->group(base_path(path: 'routes/api/v1/auth.php'));
